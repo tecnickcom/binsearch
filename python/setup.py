ff009f62be7a78b2972315612aad9c44c5c0fb0f
nicolaasuni <info@tecnick.com>
2018-06-27 11:33:44 +0100

    bump version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='binsearch',
-    version='5.0.2',
+    version='5.0.3',
     keywords=('binsearch'),
     description="Binsearch Bindings for Python",
     long_description=read('../README.md'),
