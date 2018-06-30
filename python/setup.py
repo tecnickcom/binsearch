25f254510ffdb0d1314c43a33ab59950da16c0f0
nicolaasuni <info@tecnick.com>
2018-06-30 11:00:52 +0100

    Automatically download the right Miniconda for MacOSX

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='binsearch',
-    version='5.0.3',
+    version='5.0.4',
     keywords=('binsearch'),
     description="Binsearch Bindings for Python",
     long_description=read('../README.md'),
