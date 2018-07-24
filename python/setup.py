b153be159cca74ceae8728e92b78d11be5828667
nicolaasuni <info@tecnick.com>
2018-07-24 16:23:18 +0100

    Remove inline from C functions

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='binsearch',
-    version='5.2.0',
+    version='5.2.1',
     keywords=('binsearch'),
     description="Binsearch Bindings for Python",
     long_description=read('../README.md'),
