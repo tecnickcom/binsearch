999d1ad1081a4d1ba8a437ea37c50104ddf99c5b
nicolaasuni <info@tecnick.com>
2018-08-06 11:19:05 +0100

    Replace AST secure scan library

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='binsearch',
-    version='5.2.1',
+    version='5.2.2',
     keywords=('binsearch'),
     description="Binsearch Bindings for Python",
     long_description=read('../README.md'),
