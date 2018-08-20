a1c7846e07fef5ac88f24e22669f3b1bfc156748
nicolaasuni <info@tecnick.com>
2018-08-20 10:43:00 +0100

    MOve common code to macros

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name='binsearch',
-    version='6.1.1',
+    version='6.1.3',
     keywords=('binsearch'),
     description="Binsearch Bindings for Python",
     long_description=read('../README.md'),
