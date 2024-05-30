ab865a375617d83c6ebdd1aec7f90ddee976c22e
nicolaasuni <info@tecnick.com>
2024-05-30 09:35:26 +0100

    Update golangci linter version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="binsearch",
-    version="9.4.3.0",
+    version="9.4.4.0",
     keywords=("binsearch"),
     description="Binsearch Bindings for Python",
     long_description=read("../README.md"),
