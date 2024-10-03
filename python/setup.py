0336f3d6a86333459400174f8545ed5655b948cf
nicolaasuni <info@tecnick.com>
2024-10-03 15:09:51 +0100

    Add newline in VERSION file

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="binsearch",
-    version="9.4.19.0",
+    version="9.4.20.0",
     keywords=("binsearch"),
     description="Binsearch Bindings for Python",
     long_description=read("../README.md"),
