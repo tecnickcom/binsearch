5597a72b91857275cf20528541f0cc9b5c880e41
nicolaasuni <info@tecnick.com>
2025-08-14 08:55:28 +0100

    Update dependencies

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/tecnickcom/binsearch
 
-go 1.24
+go 1.25
 
-toolchain go1.24.6
+toolchain go1.25.0
 
 require (
 	github.com/jstemmer/go-junit-report/v2 v2.1.0
--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="binsearch",
-    version="9.4.44.0",
+    version="9.4.45.0",
     keywords=("binsearch"),
     description="Binsearch Bindings for Python",
     long_description=read("../README.md"),
